a4ed8867674c9b26fa88861d03f427f34790ab05
ZeLiu1 <90260431+ZeLiu1@users.noreply.github.com>
2023-07-30 17:16:57 -0230

    path

--- preprocessor/ljspeech.py
+++ preprocessor/ljspeech.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 from text import _clean_text
 
 
-path = "/content/gdrive/MyDrive/Colab Notebooks/honour_project/dataset/MyTTSDataset_Copy"
+path = "/content/gdrive/MyDrive/Colab Notebooks/honour_project/dataset/MyTTSDataset_Copy/metadata.txt"
 
 
 def prepare_align(config):
@@ -18,7 +18,7 @@ def prepare_align(config):
     max_wav_value = config["preprocessing"]["audio"]["max_wav_value"]
     cleaners = config["preprocessing"]["text"]["text_cleaners"]
     speaker = "LJSpeech"
-    with open(os.path.join(in_dir, "metadata.txt"), encoding="utf-8") as f:
+    with open(path, encoding="utf-8") as f:
         for line in tqdm(f):
             parts = line.strip().split("|")
             base_name = parts[0]
